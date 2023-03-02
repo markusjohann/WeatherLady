f3f84fc4c635ff88a93003c739a7fbcfbfc1c0ae
Markus Johann <markus.johann.viil@gmail.com>
2023-03-02 22:06:46 +0200

    added generic

---
+++ JavaAdvancedTasks/src/Generic/Main.java
@@ -0,0 +1,46 @@
+package Generic;
+
+public class Main {
+    public static void main(String[] args) {
+        Pair<String , Integer> pair = new Pair("JavaAdvanced", 10);
+        System.out.println(pair);
+    }
+
+
+}
+
+class Pair<A, B> {
+    private A word;
+    private B value;
+
+
+    public Pair(A word, B value) {
+        this.word = word;
+        this.value = value;
+    }
+
+    public A getWord(){
+        return word;
+    }
+
+    public B getValue(){
+        return value;
+    }
+
+    public void setWord(){
+        this.word = word;
+    }
+
+    public void setValue(){
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Pair{" +
+                "word=" + word +
+                ", value=" + value +
+                '}';
+    }
+
+}
