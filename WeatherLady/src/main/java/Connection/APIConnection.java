401f33e61549115d9bee13bd4c80942f59d8985e
Markus Johann <markus.johann.viil@gmail.com>
2023-05-06 13:03:40 +0300

    few small changes

--- WeatherLady/src/main/java/Connection/APIConnection.java
+++ WeatherLady/src/main/java/Connection/APIConnection.java
@@ -1,4 +1,4 @@
-package org.example;
+package Connection;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
--- WeatherLady/src/main/java/Connection/WeatherLocationInfo.java
+++ WeatherLady/src/main/java/Connection/WeatherLocationInfo.java
@@ -1,4 +1,4 @@
-package org.example;
+package Connection;
 
 import jakarta.persistence.*;
 
